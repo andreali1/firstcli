3c290700e82a8d3a32568adffbb71333c6359026
andree <toniac91@gmail..com>
2020-09-08 01:41:51 -0500

    first cli andree

---
+++ main.py
@@ -0,0 +1,29 @@
+#>  <
+import click
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+def manda():
+    print("ktmr")
+
+@main.command()
+def saluda_usuarios():
+    print('hola mundo ')
+
+@main.command()
+def despedida_usuarios():
+    print('chau mundo ')
+
+@main.command()
+def calculadora():
+    a = int(input("ingresa 1 numero "))
+    b = int(input("ingresa otro numero "))
+    c = a + b
+    print( " el resultado de la suma es ", c)
+
+if __name__ == '__main__':
+    main()
---
+++ prueba.py
@@ -0,0 +1,7 @@
+#>  <
+import sys
+if __name__ == '__main__':
+    #print("hola mundo ")
+    if len(sys.argv) >= 1:
+        if sys.argv[1] == 'funciona':
+            print("hola perra ")
