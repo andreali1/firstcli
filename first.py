55fbecf1ade3d59b4943bec6eed3f8ac8a893389
andreali1 <toniac91@gmail.com>
2021-01-07 03:26:43 -0500

    pruebas

---
+++ first.py
@@ -0,0 +1,64 @@
+#https://docs.python.org/3.8/library/xml.etree.elementtree.html#pull-api-for-non-blocking-parsing
+#https://docs.python.org/3/library/xml.dom.minidom.html
+
+
+
+#codigo para descompresion 
+#import zipfile
+#ruta_zip = "Gestores.zip"
+#ruta_extraccion = ""
+#password = None
+#archivo_zip = zipfile.ZipFile(ruta_zip, "r")
+#try:
+    #print(archivo_zip.namelist())
+    #archivo_zip.extractall(pwd=password, path=ruta_extraccion)
+#except:
+    #pass
+#archivo_zip.close()
+
+
+
+
+#import xml.etree.ElementTree as ET
+#tree = ET.parse('datos.xml')
+#root = tree.getroot()
+#print(root)
+
+#root.tag
+#root.attrib
+#print(root.tag, root.attrib)
+
+#for f in root.iter('TABLE') :
+  #print(f.attrib)
+  #print("")
+  
+
+  
+  #for u in root.iter('ROWDATA'):
+    #print(u.attrib)
+    #for g in root.iter('ROW'):
+      #print(g.attrib)
+    #for y in root.iter('ROW'):
+      #print(y.attrib)
+  #print(g.attrib)
+#print("\n holi ")
+
+
+import xml.etree.ElementTree as ET
+tree = ET.parse('datos.xml')
+root = tree.getroot()
+#print(root)
+
+root.tag
+root.attrib
+#for h in root:
+  #print(h.attrib )
+
+
+for f in root.iter('TABLE'):
+  print(f.attrib['attrname'])
+  print(len(f.attrib['attrname']))
+
+  #for j in root.iter('ROWDATA'):
+    #print(j.attrib)
+
